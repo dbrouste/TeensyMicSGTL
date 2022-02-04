f235654234567bf981748a642d91f1bf4dc37a23
dbrouste <49265712+dbrouste@users.noreply.github.com>
2022-02-04 23:13:42 +1100

    Update main.cpp

--- Platformio project/src/main.cpp
+++ Platformio project/src/main.cpp
@@ -1,65 +1,25 @@
-
-// #include <core_pins.h> //because f_cpu_actual error
-// #include <Audio.h>
-// #include <Wire.h>
-// #include <SPI.h>
-// #include <SD.h>
-// #include <SerialFlash.h>
-
-
-
-
-// // GUItool: begin automatically generated code
-// //AudioInputI2S            i2s1;     
-// AudioSynthWaveformSine   sine1;      
-// AudioOutputUSB           usb1;           
-// //AudioConnection          patchCord1(i2s1, 0, usb1, 0);
-// AudioConnection          patchCord1(sine1, 0, usb1, 0);
-// AudioOutputI2S out1;
-// //AudioControlSGTL5000     sgtl5000_1;     
-// // GUItool: end automatically generated code
-
-// //const int myInput = AUDIO_INPUT_LINEIN;
-// //const int myInput = AUDIO_INPUT_MIC;
-
-// void setup() {
-//   // Audio connections require memory to work.  For more
-//   // detailed information, see the MemoryAndCpuUsage example
-//   AudioMemory(12);
-
-//   // Enable the audio shield, select input, and enable output
-//   //sgtl5000_1.enable();
-//   //sgtl5000_1.inputSelect(myInput);
-
-//   sine1.amplitude(1.0);
-//   sine1.frequency(15000);
-//   //sgtl5000_1.volume(1.0);
-// }
-
-// void loop() {
-// }
-
 #include <Audio.h>
-
-// GUItool: begin automatically generated code
-AudioOutputI2S out1;
-AudioSynthWaveformSine   sine1;          //xy=265.0000305175781,430.0000629425049
-AudioSynthNoiseWhite     noise1;         //xy=265.00000762939453,480.00001525878906
-AudioOutputUSB           usb1;           //xy=454.0000114440918,462.00001430511475
-AudioConnection          patchCord1(sine1, 0, usb1, 0);
-AudioConnection          patchCord2(noise1, 0, usb1, 1);
-// GUItool: end automatically generated code
+#include <Wire.h>
+#include <SPI.h>
+#include <SD.h>
+#include <SerialFlash.h>
 
 
+
+AudioInputI2S            i2s1;
+AudioOutputUSB           usb1;
+AudioConnection          patchCord1(i2s1, 0, usb1, 0);
+AudioConnection          patchCord2(i2s1, 1, usb1, 1);
+AudioControlSGTL5000     sgtl5000_1;
+
+const int myInput = AUDIO_INPUT_MIC;
+//const int myInput = AUDIO_INPUT_LINEIN;
 
 void setup() {
   AudioMemory(12);
-  sine1.frequency(400);
-  sine1.amplitude(0.5);
-  noise1.amplitude(0.5);
-
+  sgtl5000_1.enable();
+  sgtl5000_1.inputSelect(myInput);
 }
 
 void loop() {
-
 }
